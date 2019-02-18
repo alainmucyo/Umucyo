cc41a2de2a484a95c3ff669655fa4bd723839d7c
Alain MUCYO <alainmucyo3@gmail.com>
2019-02-18 07:56:23 +0200

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -189,3 +189,13 @@
 </body>
 
 </html>
+<script src="asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Plugin JavaScript -->
+<script src="asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="asset('vendor/scrollreveal/scrollreveal.min.js') }}"></script>
+<script src="asset('vendor/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/creative.min.js"></script>
